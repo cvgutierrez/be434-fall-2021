bdbf3eb82923b481f39da678a4f600432220d624
cvgutierrez <cvgutierrez@email.arizona.edu>
2021-10-27 10:39:27 -0700

    done in class

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -6,7 +6,6 @@ Purpose: Rock the Casbah
 """
 
 import argparse
-import os
 
 
 # --------------------------------------------------
@@ -38,13 +37,7 @@ def get_args():
 
     args = parser.parse_args()
     if args.kmer < 1:
-        parser.error(f'--kmer "{args.kmer}" must be greater than 0')
-    
-    if os.path.isfile(args.FILE1.name) == False:
-        parser.error(f"argument FILE1: can't open '{args.FILE1.name}'")
-    
-    if os.path.isfile(args.FILE2.name) == False:
-        parser.error(f"argument FILE2: can't open '{args.FILE2.name}'")
+        parser.error(f'--kmer "{args.kmer}" must be > 0')
 
     return args
 
@@ -55,49 +48,76 @@ def main():
 
     args = get_args()
 
-    file1_dictionary = Dictionaries(args.FILE1, args.kmer)
-    file2_dictionary = Dictionaries(args.FILE2, args.kmer)
+    kmers1 = count_kmers(args.FILE1, args.kmer)
+    kmers2 = count_kmers(args.FILE2, args.kmer)
 
-    Similarities(file1_dictionary, file2_dictionary)
-    
+    for common in set(kmers1).intersection(set(kmers2)):
+        print('{:10}{:6}{:6}'.format(common, kmers1.get(common),
+                                     kmers2.get(common)))
 
-# --------------------------------------------------
-def Dictionaries(file, kmer):
-    """Create dictionaries of kmers with number of occurances"""
+    # for kmer in kmers1:
+    #     if kmer in kmers2:
+    #         print(kmer, kmers1.get(kmer), kmers2.get(kmer))
 
-    file_Contents = ''.join(file.read().split())
-    # print(file_Contents)
+    # file1_dictionary = Dictionaries(args.FILE1, args.kmer)
+    # file2_dictionary = Dictionaries(args.FILE2, args.kmer)
 
-    file_Dictionary = dict()
-
-    listOfKmers = []
-    for i in range (0, len(file_Contents) -(kmer-1)):
-        listOfKmers.append(file_Contents[i:i+kmer])
-        print(file_Contents[i:i+kmer])
-
-    for index in range(len(listOfKmers)):
-        if listOfKmers[index] in file_Dictionary:
-            file_Dictionary[listOfKmers[index]] = file_Dictionary[listOfKmers[index]] + 1
-        else:
-            file_Dictionary[listOfKmers[index]] = 1
-
-    # print(file_Dictionary)
-
-    return file_Dictionary
+    # Similarities(file1_dictionary, file2_dictionary)
 
 
 # --------------------------------------------------
-def Similarities(file1, file2):
-    """Find similarities and print the occurances"""
+def find_kmers(seq, k):
+    """ Find k-mers in string """
 
-    similarKmers = []
-    for key in file1:
-        if key in file2:
-            similarKmers.append(key)
-            if key in similarKmers:
-                print(f'{key} {file1.get(key):10} {file2.get(key):5}')
+    n = len(seq) - k + 1
+    return [] if n < 1 else [seq[i:i + k] for i in range(n)]
 
-    
+
+# --------------------------------------
+def count_kmers(file, k):
+    '''Makes dictionaries of kmers for each file'''
+
+    kmers = {}
+    for line in file:
+        for word in line.split():
+            for kmer in find_kmers(word, k):
+                if kmer not in kmers:
+                    kmers[kmer] = 0
+                kmers[kmer] += 1
+
+    return kmers
+
+
+# # --------------------------------------------------
+# def Dictionaries(file, kmer):
+#     """Create dictionaries of kmers with number of occurances"""
+
+#     file_Dictionary = dict()
+
+#     listOfKmers = []
+#
+#   for line in file:
+#       for i in range(line - kmer + 1):
+#         listOfKmers.append(file[i:i + kmer])
+
+#     for index in range(len(listOfKmers)):
+#         if listOfKmers[index] in file_Dictionary:
+#             file_Dictionary[listOfKmers[index]] = file_Dictionary[listOfKmers[index]] + 1
+#         else:
+#             file_Dictionary[listOfKmers[index]] = 1
+
+#     return file_Dictionary
+
+# # --------------------------------------------------
+# def Similarities(file1, file2):
+#     """Find similarities and print the occurances"""
+
+#     similarKmers = []
+#     for key in file1:
+#         if key in file2:
+#             similarKmers.append(key)
+#             if key in similarKmers:
+#                 print(f'{key}{file1.get(key):10}{file2.get(key):5}')
 
 # --------------------------------------------------
 if __name__ == '__main__':
