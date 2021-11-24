b63ead4033dfe6edddc019a05f107aa375f755ee
cvgutierrez <cvgutierrez@email.arizona.edu>
2021-11-24 10:00:56 -0700

    fixed idk statement

--- assignments/03_solfege/solfege.py
+++ assignments/03_solfege/solfege.py
@@ -31,29 +31,26 @@ def main():
 
     args = get_args()
     text = args.text
-
     Song = {
-        'Do':'A deer, a female deer', 
-        'Re':'A drop of golden sun', 
-        'Mi':'A name I call myself', 
-        'Fa':'A long long way to run', 
-        'Sol':'A needle pulling thread', 
-        'La':'A note to follow sol', 
+        'Do':'A deer, a female deer',
+        'Re':'A drop of golden sun',
+        'Mi':'A name I call myself',
+        'Fa':'A long long way to run',
+        'Sol':'A needle pulling thread',
+        'La':'A note to follow sol',
         'Ti':'A drink with jam and bread'
         }
-
     for index in range(len(text)):
         if text[index] in Song:
             print((text[index]+','), Song.get(text[index]), end='')
             print()
         else:
-            print("I don't know", text[index])
-    
+            print("I don't know", '"' + text[index] + '"')
     # for index in range(len(text)):
     #     print((text[index]+','), Song.get(text[index], ("I don't know " + text[index])),end='')
     #     print()
     # Can't figure out how to just print I don't know Foo instead of Foo, I don't know Foo
-        
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
